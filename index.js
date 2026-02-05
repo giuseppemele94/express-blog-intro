40b6400214fcf09c44a1e2a22fcf044b2ac2377e
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-05 14:57:09 +0100

    add rotta base

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello"); 
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+
+// attivazione della cartella public per uso file statici
+app.use(express.static('public'));
+
+//rotta basic index.js
+app.get('/', (req,res)=> {
+    res.send("<h1>Server del mio blog</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`); 
+})
