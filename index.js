35f93f0d6e57103924761ab01e6e2a0f4f1304af
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-05 15:06:34 +0100

    struttura array di oggetti

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 
 // attivazione della cartella public per uso file statici
 app.use(express.static('public'));
 
+
+//array dei post 
+const posts = [
+    {
+        title: "",
+        content: "",
+        image: "",
+        tags: ["", ""]
+    },
+     {
+        title: "",
+        content: "",
+        image: "",
+        tags: ["", ""]
+    },
+     {
+        title: "",
+        content: "",
+        image: "",
+        tags: ["", ""]
+    },
+     {
+        title: "",
+        content: "",
+        image: "",
+        tags: ["", ""]
+    },
+     {
+        title: "",
+        content: "",
+        image: "",
+        tags: ["", ""]
+    },
+]
+
 //rotta basic index.js
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
     res.send("<h1>Server del mio blog</h1>")
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`); 
+    console.log(`App listening on port ${port}`);
 })
